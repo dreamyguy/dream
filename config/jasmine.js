77855b059b0d0a440925572922db2864591d0c92
dreamyguy <i@dreamyguy.com>
2015-09-10 13:14:51 +0200

    Exclude some other things from Jasmine

--- config/jasmine.js
+++ config/jasmine.js
@@ -2,7 +2,9 @@ module.exports = {
     src : [
         'app/js/**/*.js',
         // exclude jquery from jasmine
-        '!app/js/jquery*.js'
+        '!app/js/jquery*.js',
+        '!app/js/chartist.min.js',
+        '!.grunt/*.js'
     ],
     options : {
         specs : 'app/js/**/*spec.js',
